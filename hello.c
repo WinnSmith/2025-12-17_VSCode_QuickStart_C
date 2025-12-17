297533afc402bddded1c5270d50a50a1ab5feff3
WinnSmith <92936673+WinnSmith@users.noreply.github.com>
2025-12-17 15:56:53 +0800

    Second Commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<math.h>
+#include<stdio.h>
+int main() {
+    double num, result;
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+    result = sqrt(num);
+    printf("The square root of %.2f is %.2f\n", num, result);
+    return 0;
+}
