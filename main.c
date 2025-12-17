66fa0b322c65a7c64e7627ae17d62089b986942b
WinnSmith <lamdaatkyun@outlook.com>
2025-12-17 11:06:44 +0800

    First Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 5;
+    int sum = num1 + num2;
+
+    printf("第一个数: %d\n", num1);
+    printf("第二个数: %d\n", num2);
+    printf("两数之和: %d\n", sum);
+
+    return 0;
+}
